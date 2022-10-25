fae6151ff42c8bbe09796361e973c87a751aad8f
StupidRepo <flavouredtexts@icloud.com>
2022-10-25 14:53:44 +0100

    Textures re-added! Damn, Forge is so much better than Fabric. Although, the variable names show as p_[some numbers], which gets annoying.

--- src/main/java/com/stupidrepo/invisibilityhat/items/ModItems.java
+++ src/main/java/com/stupidrepo/invisibilityhat/items/ModItems.java
@@ -9,7 +9,7 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, InvisibilityHat.MOD_ID);
-    public static final RegistryObject<Item> THINGY = ITEMS.register("thingy", InvisibilityHatItem::new);
+    public static final RegistryObject<Item> INVIS_HAT = ITEMS.register("invis_hat", InvisibilityHatItem::new);
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
