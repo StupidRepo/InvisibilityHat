3b2829ee9e3a12132d8ad34edef7406a7ee58352
StupidRepo <flavouredtexts@icloud.com>
2022-10-25 11:29:24 +0100

    Oh god. Still moving to Forge!

---
+++ src/main/java/com/stupidrepo/invisibilityhat/InvisibilityHat.java
@@ -0,0 +1,35 @@
+package com.stupidrepo.invisibilityhat;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(InvisibilityHat.MOD_ID)
+public class InvisibilityHat {
+    public static final String MOD_ID = "invisibilityhat";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public InvisibilityHat() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("CommonSetupEvent");
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            LOGGER.info("ClientSetupEvent");
+        }
+    }
+}
